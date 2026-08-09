7cbd39b5d5456fdcfd8de9b39b820a0da15989c1
witprix <witprix@gmail.com>
2026-08-09 09:58:54 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 const CACHE_NAME = 'pdf-toolkit-cache-v1';
 const ASSETS_TO_CACHE = [
     './',
-    './index.html',
+    './pdf3.html',
     './manifest.json',
     './catppuccin--pdf.png',
     // External Libraries
